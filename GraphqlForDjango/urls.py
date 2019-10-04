8811f19e0251dc62087886a5e7f4ad3dbdbb8075
fenshitianyue <abzanda@163.com>
2019-10-04 12:38:31 +0800

    update url set

--- GraphqlForDjango/urls.py
+++ GraphqlForDjango/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from . import testdb
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^register_form/$', testdb.register_form),
+    url(r'^register/$', testdb.register),
 ]
