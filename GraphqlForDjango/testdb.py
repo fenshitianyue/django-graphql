d013a8f923e652e78fc568ff12b89fc606646530
fenshitianyue <abzanda@163.com>
2019-10-04 16:17:54 +0800

    update view code

--- GraphqlForDjango/testdb.py
+++ GraphqlForDjango/testdb.py
@@ -3,6 +3,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from datetime import date
+from datetime import datetime
 
 from fakedata.models import User  # 引入User表句柄
 from fakedata.models import Comment  # 引入Comment表句柄
@@ -21,14 +22,14 @@ def register(request):
     '''
     request.encoding = 'utf-8'
     if 'UserName' not in request.GET:
-        message = "请填写内容后提交！"
+        response = "请填写内容后提交！"
     user_name = request.GET['UserName']
     passwd = request.GET['PassWord']
     Email = request.GET['Email']
     User.objects.create(name=user_name, password=passwd, email=Email, register_date=date.today())
 
-    message = "<p>注册成功！</p>"
-    return HttpResponse(message)
+    response = "<p>注册成功！</p>"
+    return HttpResponse(response)
 
 def get_user(request):
     '''
@@ -40,17 +41,39 @@ def get_user(request):
         response += var.name + " | " + var.password + " | " + var.email + "<br>"
     return HttpResponse("<p>" + response + "</p>")
 
+def push_comment_form(request):
+    return render_to_response('comment_form.html')
+
 def push_comment(request):
     '''
     发表评论
     '''
-    pass
+    request.encoding = 'utf-8'
+    if 'UserName' not in request.GET:
+        message = "请填写内容后提交！"
+    user_name = request.GET['UserName']
+    message = request.GET['Message']
+
+    # 获取作者中间模型
+    user_tmp = User.objects.get(name=user_name)
+    # 添加评论,并获取评论中间模型
+    comment_tmp = Comment.objects.create(data=message)
+    # 向 user_comment_relationship 表添加数据
+    UserComment.objects.create(user=user_tmp, comment=comment_tmp, pushed_at=datetime.now())
+
+    response = "<p>注册成功！</p>"
+    return HttpResponse(response)
 
 def get_comment(request):
     '''
     获取评论信息
     '''
-    pass
+    List = UserComment.objects.all()
+    response = ""
+    for var in List:
+        response += var.user.name + " | " + str(var.pushed_at)[0:-13] + " | " + var.comment.data + "<br>"
+    return HttpResponse("<p>" + response + "</p>")
+
 
 # def delete_comment(request):
 #     '''
