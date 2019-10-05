f1150f48a413bbce914c10e0668be4ee687d2876
fenshitianyue <abzanda@163.com>
2019-10-05 16:57:20 +0800

    update schema

--- GraphqlForDjango/schema.py
+++ GraphqlForDjango/schema.py
@@ -3,23 +3,32 @@
 from graphene_django import DjangoObjectType
 import graphene
 
-from fakedata.models import User  # 引入User表句柄
-from fakedata.models import Comment  # 引入Comment表句柄
-from fakedata.models import UserComment  # 引入UserComment表句柄
+from mygraphdata.models import User as Users
 
 class UserType(DjangoObjectType):
     class Meta:
-        model = User
+        model = Users
 
-class CommentType(DjangoObjectType):
-    class Meta:
-        model = Comment
+# 查询
+class Query(graphene.ObjectType):
+    # List 返回结果会是遍历所有查询结果
+    users = graphene.List(UserType)
+    # Field 返回结果只存在单个(其中可以添加参数：ex、pk)
+    single_user = graphene.Field(UserType, pk=graphene.String())  # 通过名字来获取指定数据
+    # 定义函数名的格式：resolve_字段
+    # **kwargs 传递参数
+    # pk:如果在字段中定义，则方法参数中必须包含
 
-class UserCommentType(DjangoObjectType):
-    class Meta:
-        model = UserComment
+    def resolve_users(self, info, **kwargs):
+        return Users.objects.all()
 
+    def resolve_single_user(self, info, pk):
+        return Users.objects.get(name=pk)
 
+# 添加
+# 修改
+# 删除
 
+schema = graphene.Schema(query=Query)
 
 
